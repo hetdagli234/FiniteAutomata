b78e493ef168a980c0a6a2d1745ef6ed45fb14f7
hetdagli234 <daglihet@gmail.com>
2024-05-05 19:21:39 +0530

    Initializing index

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,13 @@
+import { FiniteAutomate, automata } from "./stackr/machine";
+import { StateMachine } from "@stackr/sdk/machine";
+import { transitions } from "./stackr/transitions";
+
+
 // Initialize your Micro-rollup and add a State Machine to it.
+const machine = new StateMachine({
+    id: "counter",
+    stateClass: FiniteAutomate,
+    initialState: automata.A,
+    on: transitions,
+  });
 console.log("Hello, Micro-rollup! 🖖🏻");
