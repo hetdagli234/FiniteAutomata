b3d0343a906631895a621d9dc763a461cfd4f346
hetdagli234 <daglihet@gmail.com>
2024-05-06 19:53:11 +0530

    Adding actions

--- src/stackr/action.ts
+++ src/stackr/action.ts
@@ -1,5 +1,19 @@
 import { ActionSchema, SolidityType } from "@stackr/sdk";
+import { automata, machine } from "./machine";
 
 export const UpdateFiniteAutomata = new ActionSchema("update-finite-state-machine", {
-  binary: SolidityType.UINT,
-});
+  input: SolidityType.UINT,
+}
+switch(automata) {
+    case automata.A:
+      return input === '0' ? automata.C : automata.B;
+    case automata.B:
+      return input === '0' ? automata.D : automata.A;
+    case automata.C:
+      return input === '0' ? automata.A : automata.D;
+    case automata.D:
+      return input === '0' ? automata.B : automata.C;
+    default:
+      return state; // Default case to handle any unexpected state
+  }
+);
