67a99fb13899baf525b76a16ef443b83b215e13f
hetdagli234 <daglihet@gmail.com>
2024-05-15 20:19:35 +0530

    Fixing switch state

--- src/stackr/action.ts
+++ src/stackr/action.ts
@@ -3,17 +3,15 @@ import { automata, machine } from "./machine";
 
 export const UpdateFiniteAutomata = new ActionSchema("update-finite-state-machine", {
   input: SolidityType.UINT,
-}
 switch(automata) {
-    case automata.A:
-      return input === '0' ? automata.C : automata.B;
-    case automata.B:
-      return input === '0' ? automata.D : automata.A;
-    case automata.C:
+    case machine.A:
+      return input === '0' ? machine.C : machine.B;
+    case machine.B:
+      return input === '0' ? machine.D : machine.A;
+    case machine.C:
       return input === '0' ? automata.A : automata.D;
     case automata.D:
       return input === '0' ? automata.B : automata.C;
     default:
       return state; // Default case to handle any unexpected state
-  }
 );
